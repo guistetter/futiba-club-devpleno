b1a6182fc8abb6954534d329118c118b18ad4019
guistetter <guilherme_stetter@hotmail.com>
2020-09-02 14:39:01 -0300

    criando usuario

--- account.js
+++ account.js
@@ -8,9 +8,33 @@ const init = connection => {
      console.log(rows)
     res.render('home')
   })
+
   app.get('/new-account',(req,res) =>{
-    res.render('new-account')
+    res.render('new-account',{error:false})
   })
+
+  app.post('/new-account', async(req, res) => {
+    console.log(req.body)
+    //faz select do email dos users e guarda em rows
+    const [rows, fields] = await connection.execute('select * from users where email = ?', [req.body.email])
+    if(rows.length === 0){ //verifica se email já existe
+      //inserir
+      console.log('inserir')
+      const { name, email, passwd } = req.body
+      await connection.execute('insert into users(name, email, passwd) values(?,?,?)',[
+        name, 
+        email, 
+        passwd
+      ])
+      res.redirect('/')
+    }else{
+      console.log('deu xabu')
+      res.render('new-account', {
+        error: 'Usuário já existente'
+      })
+    }
+  })
+
   return app 
 }
 
