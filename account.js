ac55798f03c510d03763639b9d96b2cbf3c6715d
guistetter <guilherme_stetter@hotmail.com>
2020-09-02 14:08:02 -0300

    criando rota para criar usuario

---
+++ account.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express.Router()
+
+const init = connection => {
+
+  app.get('/', async (req, res) =>{
+     const [rows, fields] = await connection.execute('select * from users')
+     console.log(rows)
+    res.render('home')
+  })
+  app.get('/new-account',(req,res) =>{
+    res.render('new-account')
+  })
+  return app 
+}
+
+module.exports = init
