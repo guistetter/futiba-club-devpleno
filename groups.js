769ceeeb006c279341d7157f596c8c744f7b4b01
guistetter <guilherme_stetter@hotmail.com>
2020-09-04 11:31:49 -0300

    criando middleware para verificar se usuario esta logado, se nao estiver nao tem acesso a criar grupos

--- groups.js
+++ groups.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express.Router()
 
 const init = connection =>{
+  //middleware para autenticar usuario, se nao existir nao pode criar grupo
+  app.use((req, res, next)=> {
+    if(!req.session.user){
+      res.redirect('/')
+    } else {
+      next()
+    }
+  })
   app.get('/', async(req, res) => {
     const [groups, fields] = await connection.execute('select * from grupos')
     res.render('groups',{
