d9afd7de721a447ee40c7a4b8ceadc951d21d99d
guistetter <guilherme_stetter@hotmail.com>
2020-09-04 13:45:53 -0300

    criando rota join, logica para fazer a solicitacao para entrar em um grupo

--- groups.js
+++ groups.js
@@ -20,6 +20,23 @@ const init = connection =>{
       groups: groups
     })
   })
+  //rota para pedir solicitação para entrar no grupo
+  app.get('/:id/join', async(req, res) => {
+    const [rows, fields] = await connection.execute('select * from groups_users where user_id = ? and group_id = ?',[
+      req.session.user.id, //id do usuario logado
+      req.params.id //id do grupo no qual to pedindo pra entrar 
+    ])
+    if(rows.length > 0){
+      res.redirect('/groups') //se for maior ele já está lá
+    } else {
+      await connection.execute('insert into groups_users(group_id, user_id, role) values(?,?,?)',[
+        req.params.id,
+        req.session.user.id,
+        'pending'
+      ])
+      res.redirect('/groups')
+    }
+  })
 
   app.post('/', async(req, res)=>{
    const [inserted, insertedFields] = await connection.execute('insert into grupos(name) values(?)',[
