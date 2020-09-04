9cebf4ecfefa21be07e6b8adaaedb6be81b5ccbd
guistetter <guilherme_stetter@hotmail.com>
2020-09-04 11:38:56 -0300

    criando rota post, inserindo novo grupo no banco de dados

--- groups.js
+++ groups.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express.Router()
 
 const init = connection =>{
+
   //middleware para autenticar usuario, se nao existir nao pode criar grupo
   app.use((req, res, next)=> {
     if(!req.session.user){
@@ -10,13 +11,23 @@ const init = connection =>{
       next()
     }
   })
+
   app.get('/', async(req, res) => {
     const [groups, fields] = await connection.execute('select * from grupos')
     res.render('groups',{
       groups: groups
     })
   })
+
+  app.post('/', async(req, res)=>{
+    await connection.execute('insert into grupos(name) values(?)',[
+      req.body.name
+    ])
+    res.redirect('/groups')
+  })
+
   return app
+  
 }
 
 module.exports = init
